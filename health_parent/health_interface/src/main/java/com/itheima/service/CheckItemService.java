45d36d7426ef660e524bca9d5a4f5fe06309714b
zhangkaijin <1809400941@qq.com>
2022-12-25 15:33:06 +0800

    分页查询service接口

--- health_parent/health_interface/src/main/java/com/itheima/service/CheckItemService.java
+++ health_parent/health_interface/src/main/java/com/itheima/service/CheckItemService.java
@@ -1,8 +1,11 @@
 package com.itheima.service;
 
+import com.itheima.entity.PageResult;
+import com.itheima.entity.QueryPageBean;
 import com.itheima.pojo.CheckItem;
 
 public interface CheckItemService {
 
     public void add(CheckItem checkItem);
+    public PageResult pageQuery(QueryPageBean queryPageBean);
 }
